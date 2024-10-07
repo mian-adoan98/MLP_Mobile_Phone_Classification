6510b8a8d5ad537c26bb481d1b03c67c66a9bcb3
mian-adoan98 <mian.adoan98@outlook.com>
2024-10-08 00:54:07 +0200

    created new files for processing images and downloading storing images + modified code for extracting image data from amazon webpage + challenging with visualising mobile images + preparing mobile images for training and validation set for model training

--- webscraping/webscraping_main.py
+++ webscraping/webscraping_main.py
@@ -7,30 +7,66 @@ from bs4 import BeautifulSoup
 # from webscraping import webscraper_tool_man
 from webscraper_tool_man import Webscraper, Images, Labels
 
-
-# define the requirements for extracting labels from web data
+# urls cosntants
 orange_url = "https://www.orange.lu/en/smartphones/?device_brand=5443%2C5463%2C6203%2C5468%2C6196"
-orange_response = requests.get(orange_url)
+amazon_url = "https://www.amazon.com.be/-/en/s?i=electronics&rh=n%3A27862520031%2Cp_123%3A110955%7C146762%7C329744%7C338933%7C46655&dc&fs=true&language=en&qid=1728339313&rnid=91049096031&ref=sr_nr_p_123_5&ds=v1%3AgO9ITbY27GiGtlO5Y0Ky%2BeW1wIFpSXrWqeL5%2BaZ024k"
 
+# ## Orange_url --- --------------------------------------------------------------------------------------------------------------
+# # define the requirements for extracting labels from web data
+# orange_response = requests.get(orange_url)
+
+# # create webscraper object --> check the status, create a parser
+orange_scraper = Webscraper(orange_response)
+# # orange website
+# orange_html = amazon_scraper.check_status_request()
+# amazon_soup = amazon_scraper.create_parser(orange_html)
+
+# # print(amazon_soup)
+
+# # create image object --> extract images from html page
+# all_images = amazon_soup.find_all("img")
+# image_extractor = Images(all_images)
+# images_urls = image_extractor.extract_images(orange_url)
+# image_df = image_extractor.store_image_df(images_urls)
+
+# # create a labels object --> extract label from html page
+# all_labels = amazon_soup.find_all("span")
+# label_extractor = Labels(all_labels)
+# print(all_labels)
+
+
+# # save the image data as a csv_file format
+# save_file = lambda filename, df: df.to_csv(filename)
+# save_file("image_data2.csv", image_df)
+
+# print(sys.path)
+
+## Amazon -------------------------------------------------------------------------------------------------------------------
+# define a webscraper object + create response --> extract data from aamazon website
+amazon_response = requests.get(amazon_url)
 
 # create webscraper object --> check the status, create a parser
-webscraper = Webscraper(orange_response)
-orange_html = webscraper.check_status_request()
-orange_soup = webscraper.create_parser(orange_html)
+amazon_scraper = Webscraper(amazon_response)
+# orange website
+amazon_html = amazon_scraper.check_status_request()
+amazon_soup = amazon_scraper.create_parser(amazon_html)
 
-# print(orange_soup)
+# amazon website
+amazon_html = amazon_scraper.check_status_request()
+print(amazon_html)
+
+# print(amazon_soup)
 
 # create image object --> extract images from html page
-all_images = orange_soup.find_all("img")
+all_images = amazon_soup.find_all("img")
 image_extractor = Images(all_images)
 images_urls = image_extractor.extract_images(orange_url)
 image_df = image_extractor.store_image_df(images_urls)
 
 # create a labels object --> extract label from html page
-all_labels = orange_soup.find_all("span")
+all_labels = amazon_soup.find_all("span")
 label_extractor = Labels(all_labels)
 print(all_labels)
-
 
 # save the image data as a csv_file format
 save_file = lambda filename, df: df.to_csv(filename)
@@ -38,7 +74,8 @@ save_file("image_data2.csv", image_df)
 
 print(sys.path)
 
-# ERROR : 
-# 
-# - TypeError: Images.store_image_df() takes 1 positional argument but 2 were given (line 28)
-# #
+
+# # ERROR : 
+# # 
+# # - TypeError: Images.store_image_df() takes 1 positional argument but 2 were given (line 28)
+# # #
